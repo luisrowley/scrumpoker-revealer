14905a0a6043373d67e39ae8eb884f507f07069c
luisdebenito <lud@sitecore.net>
2024-01-20 15:04:02 +0100

    script reformat to detect DOM elements

--- revealer.js
+++ revealer.js
@@ -1,8 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const flipCards = document.querySelectorAll('.flip-card');
-  
-    flipCards.forEach(function (flipCard) {
-      flipCard.classList.add('flip-card-transform');
-    });
-  });
-  
+console.log('Extension loaded');
+
+function run() {
+    var intervalID = setInterval(function() {
+        const flipCards = document.querySelectorAll('.flip-card');
+        if (flipCards.length > 0) {
+            console.log('flipCards loaded '+ flipCards);
+            flipCards.forEach(function (flipCard) {
+                flipCard.classList.add('flip-card-transform');
+            });
+            clearInterval(intervalID);
+        }
+    }, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", run);
